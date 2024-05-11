9b393051052aa4903f78716bfcd8daff89c77eb4
I Made Purnama Yasa <62276883+imadepurnamayasa@users.noreply.github.com>
2024-05-12 07:48:49 +0800

    CHANGE

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -2,7 +2,11 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Gate;
+use Laravel\Nova\Dashboards\Main;
+use Laravel\Nova\Menu\MenuItem;
+use Laravel\Nova\Menu\MenuSection;
 use Laravel\Nova\Nova;
 use Laravel\Nova\NovaApplicationServiceProvider;
 
@@ -16,6 +20,48 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     public function boot()
     {
         parent::boot();
+
+        Nova::mainMenu(function (Request $request) {
+            return [
+                MenuSection::dashboard(Main::class)->icon('chart-bar'),
+
+                MenuSection::make('References', [                    
+                    MenuItem::make('Organisation Types', 'resources/ref-organisation-types'),
+                    MenuItem::make('Employee Types', 'resources/ref-employee-types'),
+                    MenuItem::make('Leave Types', 'resources/ref-leave-types'),
+                    MenuItem::make('Event Types', 'resources/ref-event-types'),
+                    MenuItem::make('Payment Method', 'resources/ref-payment-methods'),
+                ])->icon('collection')->collapsable(),
+
+                MenuSection::make('Organisations', [
+                    MenuItem::make('Organisations', 'resources/organisations'),
+                    MenuItem::make('People', 'resources/people'),
+                    MenuItem::make('Addresses', 'resources/addresses'),
+                    MenuItem::make('Building', 'resources/buildings'),
+                    MenuItem::make('Wards', 'resources/wards'), 
+                    MenuItem::make('Rooms', 'resources/rooms'),    
+                    MenuItem::make('Beds', 'resources/beds'),                
+                ])->icon('collection')->collapsable(),
+
+                MenuSection::make('Employees', [
+                    MenuItem::make('Employees', 'resources/employees'),
+                ])->icon('collection')->collapsable(),
+
+                MenuSection::make('Customers', [
+                    MenuItem::make('Customers', 'resources/customers'),
+                ])->icon('collection')->collapsable(),
+
+                MenuSection::make('Projects', [
+                    MenuItem::make('Projects', 'resources/projects'),
+                ])->icon('collection')->collapsable(),
+
+                MenuSection::make('Users', [
+                    MenuItem::make('Users', 'resources/users'),
+                    MenuItem::make('Permissions', 'resources/permissions'),
+                    MenuItem::make('Roles', 'resources/roles'),
+                ])->icon('collection')->collapsable(),
+            ];
+        });
     }
 
     /**
